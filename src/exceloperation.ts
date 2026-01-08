66bced5b253a0430de7e2cb57fcb8b2c18390c1a
Darshanreddy M G <a829033@corp.alldata.net>
2026-01-08 17:48:49 +0530

    added new method

--- src/exceloperation.ts
+++ src/exceloperation.ts
@@ -2,3 +2,8 @@ export const excelOperation = (column: string, row: string): string => {
     const input = `in excel file ${column} and ${row}`
     return input.trim().toUpperCase();
 }
+
+export const excelOperations = (column: string, row: string): string => {
+    const input = `in excel file ${column} and ${row}`
+    return input.trim().toUpperCase();
+}
